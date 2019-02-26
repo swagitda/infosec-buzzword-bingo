06fbd6766e7c579ac87bc90370ab3cf53a5c270a
Kelly Shortridge <swagitda@users.noreply.github.com>
2019-02-26 18:57:30 -0500

    more verbs
    added some vanilla security verbs since variety is the spice of life

--- cyber-taglines.py
+++ cyber-taglines.py
@@ -3,7 +3,6 @@ import random
 s_nouns = ["Visibility", "Platform", "Solution", "Threat Hunting", "Machine Learning", "AI", "Insight", "Workflow", "Discovery", "Blockchain", "Intelligence", "Context", "Orchestration", "Runtime"]
 p_nouns = ["Threats", "Anomalies", "Zero-Days", "IoT", "Containers", "Workflows", "Insights"]
 adj = ["Cyber", "Automated", "Real-time", "Continuous", "Threat", "Advanced", "Automated", "Comprehensive", "Cyber", "Contextual", "Behavioral", "Scalable", "Seamless", "Simplified", "Unknown", "Orchestrated", "Actionable", "Deep", "Unified", "Proactive", "Adaptive", "Collaborative", "Intuitive", "Optimized", "Patented", "Cloud-based", "Cloud-native", "End-to-end", "Intelligent", "Next-gen", "Sophisticated", "Instant", "Real-world", "Customizable", "Holistic"]
-s_verbs = ["Automates", "Automating", "Simplifies", "Empowers", "Orchestrates", "Discovers", "Prioritizes", "Optimizes", "Delivers", "Stops"]
-p_verbs = ["Automate", "Simplify", "Empower", "Orchestrate", "Discover", "Prioritize", "Optimize", "Deliver", "Stop"]
+s_verbs = ["Automates", "Automating", "Simplifies", "Empowers", "Orchestrates", "Discovers", "Prioritizes", "Optimizes", "Delivers", "Stops", "Detects", "Prevents"]
 
 print (random.choice(adj),random.choice(adj),random.choice(s_nouns),random.choice(s_verbs),random.choice(adj),random.choice(p_nouns))
